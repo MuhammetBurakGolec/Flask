0920ace31fe3cf191418833c5c8c297b1588c921
Muhammet Burak Göleç <burakgolec.ofc@gmail.com>
2022-07-07 12:03:10 +0300

    Day 03
    Some Trobleshoot

--- app/views.py
+++ app/views.py
@@ -1,19 +1,29 @@
 try:
     from flask import Flask, render_template, redirect,url_for, request, make_response
+    from itsdangerous import Signer, BadSignature
 
 except ImportError:
     pass
 
 app = Flask(__name__)
-
 #@app.route("Path") 
 
 @app.route("/")
 def index():
-    name = request.cookies.get('name')
-    print('name', name)
+    signer = Signer("secret key")
+    signed_name = request.cookies.get('name')
+
+    try:
+        name = signer.unsign(signed_name).decode()
+        print('name', name)
+
+    except BadSignature:
+        print("Bad Signature")
+
+    signed_name = signer.sign("Burak")
     response = make_response("<html><body><h1>Try Flask</h1></body></html>")
-    response.set_cookie('name','burak')
+    response.set_cookie('name',signed_name)
+
     return response
 
 @app.route("/hello")
@@ -31,19 +41,20 @@ def hello_user(name):
         return redirect(url_for("admin"))
     else:
         return render_template("hello_user.html", username = name.title())
+
 """
 @app.route("/add/<int:num1>/<int:num2>")
 def add(num1,num2):
     calc = num1+ num2
     return render_template("add.html", num1 = num1, num2 = num2, result = calc)
 """
+
 @app.route("/add") #http://localhost:PORT/add?number1=1&number2=3
 def add():
     num1 = int(request.args["number1"])
     num2 = int(request.args["number2"])
     calc = num1+ num2
     return render_template("add.html", num1 = num1, num2 = num2, result = calc)
-
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
@@ -53,6 +64,7 @@ def login():
             return redirect(url_for("hello_user", name = username))
     else:
         return render_template("login.html")
+
 @app.route("/student")
 def student():
     return render_template("student.html")
